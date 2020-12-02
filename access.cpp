1636b853b8ce0cd16e0806ef15e38947b29824f4
Raj Kumawat <raj.s.kumawat01@gmail.com>
2020-12-02 17:28:49 +0530

    Added program to define and access array

---
+++ access.cpp
@@ -0,0 +1,28 @@
+//this program is show
+//how to declare array data structure
+//and access an array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declaring array of size 5
+    int arr[5];
+
+    // defining values to the array
+    arr[0]=10;
+    arr[1]=12;
+    arr[2]=14;
+    arr[3]=34;
+    arr[4]=12;
+
+    //displaying all values of t
+    //he array using for loop
+    for(int i=0; i<5; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+
+}
