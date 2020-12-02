86be69565080ce1395a11ce0af3a4f505d95af12
Raj Kumawat <raj.s.kumawat01@gmail.com>
2020-12-02 19:47:16 +0530

    updated 2D array file

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -9,23 +9,26 @@ int main()
     int arr[5] = {14,15,15,64,45};
 
     //displaying all elements of 1D array
-    cout << arr[0] << endl;
-    cout << arr[1] << endl;
-    cout << arr[2] << endl;
-    cout << arr[3] << endl;
-    cout << arr[4] << endl;
+    cout << "1D" << endl;
+    cout << arr[0] << " ";
+    cout << arr[1] << " ";
+    cout << arr[2] << " ";
+    cout << arr[3] << " ";
+    cout << arr[4] << " ";
 
     //defining two dimensional array
-    int arr1[2][3] = {{12,13,14},{22,23,24}}
+    int arr1[2][3] = {{12,13,14},{22,23,24}};
 
     //using nested for loop
     //to display the elements of 2d array
+    cout << "\n\n2D" << endl;
     for(int i=0; i<2; i++)
     {
         for(int j=0; j<3; j++)
         {
-            cout << arr1[i][j] << endl;
+            cout << arr1[i][j] << " ";
         }
+        cout << "\n";
     }
     return 0;
 }
