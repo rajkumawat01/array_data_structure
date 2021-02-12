54b05ec39697aa9eb5dba90bb1c6db37d261163c
Raj Kumawat <raj.s.kumawat01@gmail.com>
2021-02-12 21:41:24 +0530

    Adding cpp file

---
+++ 1.declaring_array.cpp
@@ -0,0 +1,21 @@
+//simple method to declare integer type array variable
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //declaring array by defining size directly
+    int a[5];
+
+    //taking input to array by user
+    for(int i=0; i<5; i++)
+    {
+        cin >> a[i];
+    }
+
+    //displaying array
+    for(int i=0; i<5; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
